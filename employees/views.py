c745f6afbe83e41eadadb7d790835865186b93dc
shahbajkhan2023 <rk2403476@gmail.com>
2024-08-14 18:58:30 +0530

    basic_authentication

---
+++ employees/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+
+class EmployeeListView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
--- token_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from rest_framework.authtoken import views as authtoken_views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-token-auth/', authtoken_views.obtain_auth_token),
-]
